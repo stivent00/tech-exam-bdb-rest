dd3be123d6e4e2ed75a6140677bb203406784706
Daniel Perez <dspr124@gmail.com>
2020-07-15 21:41:14 -0500

    Create api_bd.sql

---
+++ DB/api_bd.sql
@@ -0,0 +1,11 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+-- Started on 2020-07-15 21:17:28
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
